2a929c5b7056f1b207e868522fe4c3787f257dce
Sara Ghali <45029076+SaraGhali@users.noreply.github.com>
2026-04-03 15:09:11 +0200

    5- create courses module => using g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { UserServiceService } from './user-service/user-service.service';
+import { CoursesModule } from './courses/courses.module';
+import { CoursesModule } from './courses/courses.module';
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, CoursesModule],
   controllers: [AppController],
   providers: [AppService, UserServiceService],
 })
